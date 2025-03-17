99a5a521681f2b215b1780d6d87ad8545633445d
maihnguyen0221 <maihnguyen0221@gmail.com>
2025-03-16 20:10:46 -0600

    commented out memoizedMovies code and added API URL

--- src/components/movielist.js
+++ src/components/movielist.js
@@ -31,26 +31,26 @@ function MovieList() {
         return <div>Loading....</div>;
     }
 
-    return (
-        <Carousel onSelect={handleSelect} className="bg-dark text-light p-4 rounded">
-          {memoizedMovies.map((movie) => (
-            <Carousel.Item key={movie._id}>
-              {/* Use Nav.Link with "as={Link}" to avoid nested anchors */}
-              <Nav.Link
-                as={Link}
-                to={`/movie/${movie._id}`}
-                onClick={() => handleClick(movie)}
-              >
-                <Image className="image" src={movie.imageUrl} thumbnail />
-              </Nav.Link>
-              <Carousel.Caption>
-                <h3>{movie.title}</h3>
-                <BsStarFill /> {movie.avgRating} &nbsp;&nbsp; {movie.releaseDate}
-              </Carousel.Caption>
-            </Carousel.Item>
-          ))}
-        </Carousel>
-      );
+    // return (
+    //     <Carousel onSelect={handleSelect} className="bg-dark text-light p-4 rounded">
+    //       {memoizedMovies.map((movie) => (
+    //         <Carousel.Item key={movie._id}>
+    //           {/* Use Nav.Link with "as={Link}" to avoid nested anchors */}
+    //           <Nav.Link
+    //             as={Link}
+    //             to={`/movie/${movie._id}`}
+    //             onClick={() => handleClick(movie)}
+    //           >
+    //             <Image className="image" src={movie.imageUrl} thumbnail />
+    //           </Nav.Link>
+    //           <Carousel.Caption>
+    //             <h3>{movie.title}</h3>
+    //             <BsStarFill /> {movie.avgRating} &nbsp;&nbsp; {movie.releaseDate}
+    //           </Carousel.Caption>
+    //         </Carousel.Item>
+    //       ))}
+    //     </Carousel>
+    //   );
     }
 
 export default MovieList;
